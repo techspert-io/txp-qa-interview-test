98f07ae2326a2679742fa373421ec568da81515f
Philip <philip.rozycki-burn@techspert.io>
2024-11-14 09:10:45 +0000

    install MUI and set up theme

---
+++ src/libs/react/theme/theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material';
+import { dataDisplayCustomizations } from './customizations/dataDisplay';
+import { feedbackCustomizations } from './customizations/feedback';
+import { inputsCustomizations } from './customizations/inputs';
+import { navigationCustomizations } from './customizations/navigation';
+import { surfacesCustomizations } from './customizations/surfaces';
+import { colorSchemes, shadows, shape, typography } from './themePrimitives';
+
+export default createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-mui-color-scheme',
+    cssVarPrefix: 'template',
+  },
+  colorSchemes,
+  typography,
+  shadows,
+  shape,
+  components: {
+    ...inputsCustomizations,
+    ...dataDisplayCustomizations,
+    ...feedbackCustomizations,
+    ...navigationCustomizations,
+    ...surfacesCustomizations,
+  },
+});
