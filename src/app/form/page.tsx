853cc310bb73f7e77c91c3c14b8b0741f2b69d0d
Philip <philip.rozycki-burn@techspert.io>
2024-11-15 16:32:51 +0000

    form layout

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,13 @@
 import { BaseBreadcrumb } from '@/libs/react/router/breadcrumbs';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 import { PageContainer } from '@toolpad/core';
 
 export default function Form() {
@@ -6,6 +15,87 @@ export default function Form() {
     <PageContainer
       title="Form"
       breadcrumbs={[BaseBreadcrumb, { title: 'Form', path: '/form' }]}
-    ></PageContainer>
+    >
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 2, sm: 2, md: 12 }}
+        display="flex"
+      >
+        <Grid size={6}>
+          <TextField
+            variant="outlined"
+            label="First name"
+            required={true}
+            sx={{ width: '100%' }}
+          />
+        </Grid>
+        <Grid size={6}>
+          <TextField
+            variant="outlined"
+            label="Last name"
+            required={true}
+            sx={{ width: '100%' }}
+          />
+        </Grid>
+        <Grid size={12}>
+          <TextField
+            variant="outlined"
+            label="Email address"
+            required={true}
+            sx={{ width: '100%' }}
+          />
+        </Grid>
+        <Grid size={4}>
+          <TextField
+            variant="outlined"
+            label="Date of birth"
+            type="date"
+            sx={{ width: '100%' }}
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+          />
+        </Grid>
+        <Grid size={4}>
+          <TextField
+            variant="outlined"
+            label="Years of experience"
+            type="number"
+            sx={{ width: '100%' }}
+          />
+        </Grid>
+        <Grid size={4}>
+          <FormControl fullWidth>
+            <InputLabel id="form-education-level-label">
+              Highest education level
+            </InputLabel>
+            <Select
+              label="Highest education level"
+              labelId="form-education-level-label"
+              sx={{ marginTop: 'auto' }}
+            >
+              <MenuItem value="high-school">High school or equivalent</MenuItem>
+              <MenuItem value="college">College</MenuItem>
+              <MenuItem value="associate-or-trade">
+                Associate or Trade qualification
+              </MenuItem>
+              <MenuItem value="bachelors">Bachelor's degree</MenuItem>
+              <MenuItem value="masters">
+                Masters or other post-graduate degree
+              </MenuItem>
+              <MenuItem value="doctorate">Doctorate</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid size={12}>
+          <Button variant="contained" sx={{ width: '100%' }}>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </PageContainer>
   );
 }
